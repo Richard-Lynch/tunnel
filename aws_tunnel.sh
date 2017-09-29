785c90b9c208fa2253b6f44f244584d34efdcb20
Ubuntu <ubuntu@ip-172-31-41-124.eu-west-1.compute.internal>
2017-09-29 20:31:13 +0000

    added remove ssh tunnel to aws

--- aws_tunnel.sh
+++ aws_tunnel.sh
@@ -12,3 +12,23 @@ awsUser="ubuntu"
 
 ssh-keygen -f "/home/$awsUser/.ssh/known_hosts" -R [localhost]:$awsPort
 ssh -i $dockerKeyLocation -p $awsPort $dockerUser@localhost
+echo "Programs listening to port $awsPort"
+sudo fuser $awsPort/tcp
+read -p "Kill all?" yn
+while true ; do
+	case $yn in
+		[Yy] | [Yy][Ee][Ss] )
+			echo "Killing"
+			sudo fuser $awsPort/tcd -k
+			break		
+			;;
+		[Nn] | [Nn][Oo] )
+			echo "Not Killing"
+			exit;;
+		*)
+			echo "Yes or no only please";;
+esac
+
+
+
+
