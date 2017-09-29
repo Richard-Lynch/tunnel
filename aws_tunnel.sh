3534e0c67c8cb0e26371e5c68e478e13d01dab08
Richard-Lynch <rich753@hotmail.com>
2017-09-29 20:12:34 +0000

    removed useless code from AWS

--- aws_tunnel.sh
+++ aws_tunnel.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# ------------- DOCKER --------------------
-# location of key to AWS server
-awsKeyLocation="./aws"
-# command needed to use awsKey isntead of password
-awsKeyCommand="" #"-i $awsKeyLocation"
-# port l
-awsPort="22222"
-# user on aws
-awsUser="ubuntu"
-# address of aws ( if ip, may need -N )
-awsAddress="ec2-54-194-71-187.eu-west-1.compute.amazonaws.com"
-# port on the docker instance to bind the tunnel to
-dockerPort="22"
-
 # ------------ AWS -------------------
 # locatio in git folder of turing public key
 dockerKeyLocation="./id_rsa"
@@ -21,6 +7,8 @@ dockerKeyLocation="./id_rsa"
 dockerUser="tcd"
 # port for docker to map to
 awsPort="22222"
+# user on aws
+awsUser="ubuntu"
 
 ssh-keygen -f "/home/$awsUser/.ssh/known_hosts" -R [localhost]:$awsPort
 ssh -i $dockerKeyLocation -p $awsPort $dockerUser@localhost
